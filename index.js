01ab21b703d2c82d5594c40bd899f6ac5c2050f8
Zahidul Haque <zahidulhaquer@gmail.com>
2025-10-08 07:51:44 +0600

    Progress 10-08-2025

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
-}
+let messageEl = document.getElementById("message-el");
 
-console.log(message);
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+
+  // console.log(message);
+}
